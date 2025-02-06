1a296abbc912a39f973dbe14b272edf2ba6cf252
janvim2008 <janvimandanisanjaybhai@gmail.com>
2025-02-06 15:06:09 +0530

    msg

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -1,6 +1,7 @@
+const app = require('../app');
 var user = require('../model/usermodel');
 
-
+login_status=0;
 exports.insert=async(req,res)=>{
 
     user.create(req.body);
@@ -10,21 +11,22 @@ exports.insert=async(req,res)=>{
 }
 
 exports.getdata=async(req,res)=>{
-    var limit=3;
-    var total_rec=await user.countDocuments();
-    var total_page=Math.ceil(total_rec/limit);
-    var page_no=req.query.page_no;
-    if(page_no==undefined)
-    {
-        page_no=1;
-    }
-    var start=(page_no-1)*limit;
-    var data =await user.find().skip(start).limit(limit);
+    // var limit=3;
+    // var total_rec=await user.countDocuments();
+    // var total_page=Math.ceil(total_rec/limit);
+    // var page_no=req.query.page_no;
+    // if(page_no==undefined)
+    // {
+    //     page_no=1;
+    // }
+    // var start=(page_no-1)*limit;
+    // var data =await user.find().skip(start).limit(limit);
+    var data =await user.find();
     res.status(200).json({
         status:'get all data',
-        data,
-        total_page,
-        page_no
+        data
+        // total_page,
+        // page_no
     })
 }
 
@@ -45,7 +47,7 @@ exports.update=async(req,res)=>{
         status:'update data',
         messge:'update successfully',
         data
-    })
+    })  
 }
 
 exports.delete=async(req,res)=>{
@@ -56,4 +58,45 @@ exports.delete=async(req,res)=>{
         messge:'delete successfully',
         data
     })
+    }
+exports.login = async(req,res)=>{
+    var data = await user.find({"email" : req.body.email})
+    if(login_status==0)
+    {
+        if(data.length==1)
+        {
+            if(data[0].password==req.body.password)
+            {
+                login_status=1;
+                res.status(200).json({
+                    status:"login successfully"
+                })
+            }
+            else{
+                res.status(200).json({
+                    status:"checik email and password"
+                })
+            }
+        }
+        else{
+            res.status(200).json({
+                status:"checik email and password"
+            })
+        }
+    }
+    else{
+        res.status(200).json({
+            status:"user already login"
+        }) 
+    }
 }
+// if (data.status === "Login successful") {
+//     // Store user data (email and ID) in localStorage
+//     localStorage.setItem("user", JSON.stringify(data.user));  // Save user data (email, id)
+    
+//     // Optionally, you could also store other data like authentication token (if you use one)
+//     alert("Login successful!");
+//     console.log("User data stored in localStorage:", data.user);
+// } else {
+//     alert(data.status);  // Show error message if login fails
+// }
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ var user = require('../controller/usercontroller')
 /* post home page. */
 router.post('/',user.insert);
 // get method
-router.get('/',user.getdata);
+router.get('/get',user.getdata);
 router.get('/update_getdata/:id',user.update_getdata);
 router.post('/update/:id',user.update);
 router.get('/delete/:id',user.delete);
+router.post('/login',user.login);
 module.exports = router;
